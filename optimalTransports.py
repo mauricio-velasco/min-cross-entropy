aece687e8906734d4b19ccaccbc36b490b499284
mauricio-velasco <mauvelasco@gmail.com>
2021-05-14 10:38:32 -0500

    new version implementation of weighted Voronoi Diagrams

--- optimalTransports.py
+++ optimalTransports.py
@@ -4,14 +4,26 @@ Created on Oct 6, 2019
 @author: mvelasco
 '''
 import numpy as np
+import pdb
 import time
 
 
 class Empirical_Measure:
-    def __init__(self, data_points_vector, distance_fn):
+    """
+    An empirical measure is determined by a list of data points (self.data_vectors)
+    in some R^n. n=self.dim. Represents the sum of Dirac deltas at these data points.
+    
+    An empirical measure is capable of:
+        -Acting on functions by integration (integrate)
+        -Find the index of nearest data point to a given new point (nearest_data_point_index).
+            the notion of distance used is specified in self.distance.
+        -Classify_nearest (for each vector on a list returns the vector in self.data_vectors closest to it)
+    
+    """
+    def __init__(self, data_points_vector):
         self.data_vectors = []
-        self.distance=distance_fn
         self.ndata_vectors = len(data_points_vector)
+        assert(self.ndata_vectors>0)
         #Receives an array of vectors corresponding to the observations x_i
         first =True
         for vector in data_points_vector:
@@ -28,27 +40,167 @@ class Empirical_Measure:
             Result.append(fn(vector)*(1/self.ndata_vectors))
         return np.sum(Result)
 
-    def nearest_data_point_index(self, vector):
-            distances = np.array([self.distance(vector, data_point) for data_point in self.data_vectors])
-            k = np.where(distances == np.min(distances))[0]
-            return(k[0])
 
+class Probability_Measure:
+    """
+    We represent probability measures via black box routine that produces independent samples from 
+    this distribution.
     
-    def classify_nearest(self, vectors_to_classify):
-        Classified_Points = [[] for k in range(self.ndata_vectors)]
-        for vector in vectors_to_classify:
-            k = self.nearest_data_point_index(vector)
-            Classified_Points[k].append(vector)
-        return Classified_Points
+    
+    A probability measure is capable of:
+        -Producing an independent sample of the random variable.
+        -Acting on functions by integration (integrate via MonteCarlo)
+    
+    """
+    
+    def __init__(self, sample_q_fn):
+        self.sample_q_fn = sample_q_fn
+        
+    def sample_q(self, numsamples):
+        return self.sample_q_fn(numsamples)
 
+    def integrate(self, fn):
+        points = self.sample_q(self.MC_points)
+        return np.average([fn(p) for p in points])
+
+
+class Weighted_Voronoi_Diagram:
+    """A weighted Voronoi diagram is specified by:
+         -a collection of centers 
+         -with a corresponding vector of weights, one per center which sum to zero (\lambdas in the article)
+         -An ambient distance function
+
+    It should be capable of:
+    (1) Given a new vector find the index of the weighted nearest center+
+    (2) Given a collection of vectors count the fraction of these points in each of the weighted voronoi cells.+
+    """
+    
+    def __init__(self, centers_array, weights_vector, distance_fn):
+        self.centers = []
+        self.distance = distance_fn
+        self.ncenters = len(centers_array)
+        #Receives an array of vectors corresponding to the centers of the Weighted Voronoi
+        first =True
+        for vector in centers_array:
+            if first:
+                first=False
+                self.dim = len(vector)
+            assert(len(vector) == self.dim)
+            self.centers.append(np.array(vector))
+        self.weights = weights_vector
+        assert(len(self.centers) == len(self.weights))
+
+        
+    def index_weighted_cell_of(self, vector):
+        #Given a new vector find the index of the weighted nearest center        
+        N = self.ncenters
+        weighted_distances = np.array([self.distance(vector, self.centers[k])-self.weights[k] for k in range(N)])
+        index = np.where(weighted_distances == np.min(weighted_distances))[0]
+        return(index[0])
+
+    def minimal_weighted_distance_to_data_point(self,vector):
+            N = self.ncenters
+            weighted_distances = np.array([self.distance(vector, self.centers[k])-self.weights[k] for k in range(N)])
+            return(np.min(weighted_distances))
+
+    def compute_array_minimal_weighted_distances(self, vectors_array):
+        distances_vector = np.zeros(len(vectors_array))
+        for k, vector in enumerate(vectors_array):
+            distances_vector[k] = self.minimal_weighted_distance_to_data_point(vector)
+        return(distances_vector)
+
+    def compute_proportions_in_cells(self, vectors_array, gradient=False):
+        #Given a collection of vectors count the fraction of these points in each of the weighted voronoi cells
+        region_counts_vector = np.zeros(self.ncenters)
+        for vector in vectors_array:
+            idx = self.index_weighted_cell_of(vector)
+            region_counts_vector[idx]+=1
+        if gradient:
+            return (-1)*(region_counts_vector/len(vectors_array)-(1/self.ncenters))
+        else:
+            return region_counts_vector/len(vectors_array)
+
+
+
+class Optimal_Transport_Finder:
+    """ This function finds an optimal transport between 
+    an empirical measure and any probability measure q specified by a sampling function."""    
+    def __init__(self, empirical_measure, probability_measure, distance_fn, num_MC):
+        #Check the input types:
+        assert(isinstance(empirical_measure,Empirical_Measure ))
+        self.empirical_measure = empirical_measure
+        assert(isinstance(probability_measure,Probability_Measure ))
+        self.probability_measure = probability_measure
+        weights = np.zeros(empirical_measure.ndata_vectors)
+        centers = empirical_measure.data_vectors
+        self.WVD = Weighted_Voronoi_Diagram(centers,weights,distance_fn)
+        self.bestLambdas = self.WVD.weights.copy()#Will keeptrack of the best lambdas so far.
+        self.num_MC = num_MC
+        self.best_objective_so_far = self.compute_objective()
+        self.best_weights_so_far = np.zeros(len(self.WVD.weights))
+        self.best_gradient_norm = 0
+        
+    def compute_objective(self):
+        """Computes the current \Psi(\lambda) with \lambda=self.WVD.weights"""
+        #Create a sample from our probability measure
+        sample_vectors_array = self.probability_measure.sample_q(self.num_MC)
+        #Evaluate the minimum weighted distance of each sampled point to the centers
+        min_distances = self.WVD.compute_array_minimal_weighted_distances(sample_vectors_array)
+        #Monte Carlo evaluation of the integral
+        return(np.average(min_distances))
+ 
+    def compute_gradient(self):
+        #Create a sample from our probability measure
+        sample_vectors_array = self.probability_measure.sample_q(self.num_MC)
+        gradient = self.WVD.compute_proportions_in_cells(sample_vectors_array, gradient=True)
+        return gradient
+
+    def do_gradient_descent(self, NumSteps, StepSize, keep_track_of_best = True, Descending_in_size=True):
+        #Do a gradient descent for NumSteps steps
+        for k in range(NumSteps):
+            weights = self.WVD.weights
+            gradient = self.compute_gradient()
+            if Descending_in_size:    
+                CurrStepSize = StepSize/(1+np.sqrt(k))#
+            else:
+                CurrStepSize = StepSize
+            #gradient step
+            new_weights = weights + CurrStepSize * gradient
+            self.WVD.weights = new_weights
+            if keep_track_of_best:
+                objective = self.compute_objective()
+                grad_norm = np.linalg.norm(gradient)
+                print("Step "+str(k)+":\n")
+                self.print_current_status(objective, grad_norm)
+                if objective > self.best_objective_so_far:
+                    self.best_objective_so_far = objective
+                    self.best_weights_so_far = new_weights
+                    self.best_gradient_norm = np.linalg.norm(gradient)
+                    
+        
+        #if we keep track of the best then we should set it as the chosen weights
+        if keep_track_of_best:
+            self.WVD.weights = self.best_weights_so_far
+
+    def print_current_status(self,curr_obj,curr_grad_norm):
+        print("_____________________________________________________________")
+        print("Curr_obj: " + str(curr_obj)+ "")
+        print("Curr_grad_norm: " + str(curr_grad_norm)+ "")
+        print("Best_obj: " + str(self.best_objective_so_far)+ "")
+        print("grad_norm_at_best: " + str(self.best_gradient_norm)+ "")
+        print("_____________________________________________________________")
+        
+
+"""PROTOTYPES FOR DISTANCE FUNCTION AND SAMPLING FUNCTION FOR SPECIFYING A DISTRIBUTION"""        
 def dist(x,y):
-    #For now uses the l2-norm but one can put here any norm
+    #Prototype for distance functions allowed. For now uses the l2-norm but one can put here any norm
     assert(len(x)==2)
     assert(len(x)==len(y))
     return np.linalg.norm(x-y)    
 
 def sample_q(numSamples):
-    #Uniform distribution in [-1,1], [-1,1]
+    #prototype of a sampling function. This is how measures are specified.
+    """returns a collection of numSamples many independent vectors unif distributed in [-1,1] x [-1,1]"""
     ResultsArray = []
     Xs = np.random.uniform(-1,1,numSamples)
     Ys = np.random.uniform(-1,1,numSamples)
@@ -56,116 +208,17 @@ def sample_q(numSamples):
         ResultsArray.append([Xs[k],Ys[k]])
     return ResultsArray
 
-class optimalTransportFinder:
-    def __init__(self, empirical_data_points, sample_q_fn, distance_fn, num_MC = 1000):
-        self.empirical_measure = Empirical_Measure(empirical_data_points, distance_fn)
-        self.currentLambdas = np.zeros(len(empirical_data_points))
-        self.bestLambdas = self.currentLambdas
-        self.num_MC = num_MC
-        self.sample_q_fn = sample_q_fn
-        self.distance = distance_fn
-        self.compute_current_state_vector()
-        self.bestPsi = self.current_Psi_scalar_value
-    
-    def weighted_nearest_data_point_index(self, vector):
-            N = self.empirical_measure.ndata_vectors
-            weighted_distances = np.array([self.distance(vector, self.empirical_measure.data_vectors[k])-self.currentLambdas[k] for k in range(N)])
-            k = np.where(weighted_distances == np.min(weighted_distances))[0]
-            return(k[0])
-
-    def weighted_classify_nearest(self, vectors_to_classify):
-        Classified_Points = [[] for k in range(self.empirical_measure.ndata_vectors)]
-        for vector in vectors_to_classify:
-            k = self.weighted_nearest_data_point_index(vector)
-            Classified_Points[k].append(vector)
-        return Classified_Points
-
-    def evaluate_phi_lambda_at_vector(self, vector):
-        number_Dta_Points = self.empirical_measure.ndata_vectors
-        weighted_distances = np.array([self.distance(vector, self.empirical_measure.data_vectors[k])-self.currentLambdas[k] for k in range(number_Dta_Points)])
-        return np.min(weighted_distances)
-
-    def evaluate_min_distance_at_vector(self, vector):
-        number_Dta_Points = self.empirical_measure.ndata_vectors
-        distances = np.array([self.distance(vector, self.empirical_measure.data_vectors[k]) for k in range(number_Dta_Points)])
-        return np.min(distances)
-
-    def compute_current_state_vector(self):
-        N = self.empirical_measure.ndata_vectors        
-        samples_q = self.sample_q_fn(self.num_MC)
-        classified_samples = self.weighted_classify_nearest(samples_q)
-        #assert(len(classified_samples)==N)
-        #First we compute the super-gradient
-        counts = [(-1)*len(res)/(self.num_MC) + (1/N) for res in classified_samples]
-        self.current_super_gradient_value = np.array(counts)
-        #Alternative computation of Psi
-        #evaluated_samples = [self.evaluate_phi_lambda_at_vector(vector) for vector in samples_q]
-        #alternativePsi = np.sum(evaluated_samples)
-
-        #Computation of Psi
-        weighted_evaluated_distances = []
-        for k in range(N):
-            for vector in classified_samples[k]:
-                weighted_evaluated_distances.append(self.distance(self.empirical_measure.data_vectors[k], vector)-self.currentLambdas[k])
-        
-        self.current_Psi_scalar_value = np.sum(weighted_evaluated_distances)/self.num_MC
-
-
-
-    def new_one_step(self, step_size):
-        self.compute_current_state_vector()
-        g = self.current_super_gradient_value        
-        self.currentLambdas = self.currentLambdas + step_size*g
-        #assert(np.abs(np.sum(self.currentLambdas))< 1e-8)
-        #assert(np.abs(self.current_Psi_scalar_value-self.current_Psi_value())<1e-2)
-        newPsi = self.current_Psi_scalar_value
-        if newPsi >= self.bestPsi:
-            self.current_super_gradient_norm = np.linalg.norm(self.current_super_gradient_value)
-            print()
-            print("Step "+str(1/step_size))
-            print("Step Size "+str(step_size))
-            print("Current Psi: "+str(self.current_Psi_scalar_value))
-            print("Current norm: "+ str(self.current_super_gradient_norm))
-            self.bestPsi = newPsi
-            self.bestLambdas = self.currentLambdas
-
-    
-    def compute_optimal_lambda(self, numSteps, epsilon = 1.0):
-        for k in range(numSteps):
-            OT.new_one_step(epsilon/(k+1))        
-        
-        print()
-        print("Computation of "+str(numSteps)+" steps completed.")
-        self.print_Results()
-        OT.currentLambdas = OT.bestLambdas
-
-    def print_Results(self):
-        print("Best Lower bound for W(q,mu): "+ str(OT.bestPsi))
-        print("Gradient norm at optimality: "+str(self.current_super_gradient_norm))
-        print("Best Lambdas achieving it: ")
-        print(OT.bestLambdas)
-        
-
 
 if __name__ == "__main__":
-
-    #1. Empirical measures:        
-    #empirical_data_points_vector = [[1,0], [0,0], [0,1]]
-    empirical_data_points_vector = [np.random.uniform(-1,1,2) for k in range(60)]
-    print(empirical_data_points_vector)
-    mu = Empirical_Measure(empirical_data_points_vector, dist)
-    classify_data_points_vector = [[1,0], [0,0], [0,1], [1.1,0], [0,0.1], [0,1.1]]
-    print(mu.classify_nearest(classify_data_points_vector))
-    #2. optimal transport between q and a given empirical measure built from MonteCarlo
-    t0 = time.time()
-    OT = optimalTransportFinder(empirical_data_points_vector, sample_q, dist, 10000)
-    OT.compute_optimal_lambda(20)
-    t1 = time.time()
-    print("Time elapsed: "+str(t1-t0))
-    print("Step one finished")
-    OT.compute_optimal_lambda(20, epsilon = 0.1)
-    t2 = time.time()
-    print("Step two finished")
-    print("Time elapsed: "+str(t2-t1))
+    #0.Weighted Voronoi diagrams:
+    N=5 #number of centers of the empirical measure
+    centers_array = [np.random.uniform(-1,1,2) for k in range(N)] #Chosen uniformly in the square [-1,1]x[-1,1]
+    empirical_measure = Empirical_Measure(centers_array)
+    #The probability measure is specified by its sampling function
+    probability_measure = Probability_Measure(sample_q)
+    #We construct the optimal transport object which carries out the gradient descent
+    OT = Optimal_Transport_Finder(empirical_measure,probability_measure,dist,num_MC=100000)
+    OT.do_gradient_descent(NumSteps=30, StepSize=0.5, keep_track_of_best=True, Descending_in_size=True)    
+    WVD = OT.WVD #This is the resulting weighted Voronoi Diagram, which encodes the optimal transport
     
     
